04af49548a62fb2f022a46c4ff7a5fa7b75e6f8f
sai <saiteja982610kommineni@gmail.com>
2024-03-31 17:11:25 +0530

    TEST2

--- Funtion.py
+++ Funtion.py
@@ -1,3 +1,8 @@
+x= float(input("enter the 1st num: "))
+y= float(input("enter the 2nd num: "))
+# def Rem(x,y):
+#     result= x%y
+#     print(f"The reminder of {x} and {y} numbers is:{result}")
 # def sum(x,y):
 #     result= x+y
 #     print(f"The sum of {x} and {y} numbers is:{result}")
@@ -21,4 +26,4 @@ def Calculator(x,y):
     print(f"The sub of {x} and {y} numbers is:{result}")
     result= x*y
     print(f"The mxn of {x} and {y} numbers is:{result}")
-#TEST123  
+#TTESTTT
